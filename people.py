fd506780340f01d0086d86ba387eebd7a57ca120
Andrew Casey <accasey@hotmail.com.au>
2020-11-07 20:31:08 +0000

    version 4

--- people.py
+++ people.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from flask import abort, make_response
+
 
 def get_timestamp() -> str:
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
@@ -26,7 +28,7 @@ PEOPLE = {
 
 
 # Create a handler for our read (GET) people
-def read():
+def read_all():
     """
     This function responds to a request for /api/people
     with the complete lists of people
@@ -35,3 +37,79 @@ def read():
     """
     # Create the list of people from the data
     return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
+
+
+def read_one(lname):
+    """
+    This function responds to a request for /api/people/{lname}
+    with one matching person from people
+    :param lname:   last name of person to find
+    :return:        person matching last name
+    """
+    # Does the person exist?
+    if lname in PEOPLE:
+        person = PEOPLE.get(lname)
+
+    else:
+        abort(404, f"Person with last name {lname} not found.")
+
+    return person
+
+
+def create(person):
+    """
+    This function creates a new person in the people structure
+    based on the passed in person data
+    :param person:  person to create in people structure
+    :return:        201 on success, 406 on person exists
+    """
+    lname = person.get("lname", None)
+    fname = person.get("fname", None)
+
+    # Does the person already exist?
+    if lname not in PEOPLE and lname is not None:
+        PEOPLE[lname] = {
+            "lname": lname,
+            "fname": fname,
+            "timestamp": get_timestamp(),
+        }
+        return make_response(f"{lname} successfully created", 201)
+
+    # Otherwise they exist, and that is an error
+    else:
+        abort(406, f"Person with last name {lname} already exists")
+
+
+def update(lname, person):
+    """
+    This function updates an existing person in the people structure
+    :param lname:   last name of person to update in the people structure
+    :param person:  person to update
+    :return:        updated person structure
+    """
+    # Does the person exist in people?
+    if lname in PEOPLE:
+        PEOPLE[lname]["fname"] = person.get("fname")
+        PEOPLE[lname]["timestamp"] = get_timestamp()
+
+        return PEOPLE[lname]
+
+    # otherwise, nope, that's an error
+    else:
+        abort(404, f"Person with last name {lname} not found")
+
+
+def delete(lname):
+    """
+    This function deletes a person from the people structure
+    :param lname:   last name of person to delete
+    :return:        200 on successful delete, 404 if not found
+    """
+    # Does the person to delete exist?
+    if lname in PEOPLE:
+        del PEOPLE[lname]
+        return make_response(f"{lname} successfully deleted", 200)
+
+    # Otherwise, nope, person to delete not found
+    else:
+        abort(404, f"Person with last name {lname} not found")

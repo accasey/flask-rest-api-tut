6c2a6a85da7e17bcd2a2ff8ee8a4126b4a9676f2
Andrew Casey <accasey@hotmail.com.au>
2020-11-08 20:34:27 +0000

    Finished tutorial #2

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
+"""
+Main module of the server file
+"""
 from flask import render_template
-import connexion
 
-# Create the application instance
-app = connexion.App(__name__, specification_dir="./")
+import config
 
+# Get the application instance
+connex_app = config.connex_app
 
 # Read the swagger.yml file to configure the endpoints
-app.add_api("swagger.yml")
+connex_app.add_api("swagger.yml")
 
 # Create a URL route in the application for '/'
-@app.route("/")
+@connex_app.route("/")
 def home():
     """
     This function just responds to the browser URL
@@ -22,5 +25,5 @@ def home():
 
 # If we are running in stand alone mond, run the application
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    connex_app.run(host="0.0.0.0", port=5000, debug=True)
     # app.run(debug=True)
--- static/js/home.js
+++ static/js/home.js
@@ -29,17 +29,14 @@ ns.model = (function () {
                     $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
                 })
         },
-        'create': function (fname, lname) {
+        'create': function (person) {
             let ajax_options = {
                 type: 'POST',
                 url: 'api/people',
                 accepts: 'application/json',
                 contentType: 'application/json',
                 dataType: 'json',
-                data: JSON.stringify({
-                    'fname': fname,
-                    'lname': lname
-                })
+                data: JSON.stringify(person)
             };
             $.ajax(ajax_options)
                 .done(function (data) {
@@ -49,18 +46,16 @@ ns.model = (function () {
                     $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
                 })
         },
-        'update': function (fname, lname) {
+        'update': function (person) {
             let ajax_options = {
                 type: 'PUT',
-                url: 'api/people/' + lname,
+                url: `api/people/${person.person_id}`,
                 accepts: 'application/json',
                 contentType: 'application/json',
                 dataType: 'json',
-                data: JSON.stringify({
-                    'fname': fname,
-                    'lname': lname
-                })
+                data: JSON.stringify(person)
             };
+            // console.log(person);
             $.ajax(ajax_options)
                 .done(function (data) {
                     $event_pump.trigger('model_update_success', [data]);
@@ -69,10 +64,10 @@ ns.model = (function () {
                     $event_pump.trigger('model_error', [xhr, textStatus, errorThrown]);
                 })
         },
-        'delete': function (lname) {
+        'delete': function (person_id) {
             let ajax_options = {
                 type: 'DELETE',
-                url: 'api/people/' + lname,
+                url: `api/people/${person_id}`,
                 accepts: 'application/json',
                 contentType: 'plain/text',
             };
@@ -91,18 +86,21 @@ ns.model = (function () {
 ns.view = (function () {
     'use strict';
 
-    let $fname = $('#fname'),
+    let $person_id = $('#person_id'),
+        $fname = $('#fname'),
         $lname = $('#lname');
 
     // return the API
     return {
         reset: function () {
+            $person_id.val('');
             $lname.val('');
             $fname.val('').focus();
         },
-        update_editor: function (fname, lname) {
-            $lname.val(lname);
-            $fname.val(fname).focus();
+        update_editor: function (person) {
+            $person_id.val(person.person_id);
+            $lname.val(person.lname);
+            $fname.val(person.fname).focus();
         },
         build_table: function (people) {
             let rows = '';
@@ -113,7 +111,11 @@ ns.view = (function () {
             // did we get a people array
             if (people) {
                 for (let i = 0; i < people.length; i++) {
-                    rows += `<tr><td class="fname">${people[i].fname}</td><td class="lname">${people[i].lname}</td><td>${people[i].timestamp}</td></tr>`;
+                    rows += `<tr data-person-id="${people[i].person_id}">
+                        <td class="fname">${people[i].fname}</td>
+                        <td class="lname">${people[i].lname}</td>
+                        <td>${people[i].timestamp}</td>
+                    </tr>`;
                 }
                 $('table > tbody').append(rows);
             }
@@ -136,6 +138,7 @@ ns.controller = (function (m, v) {
     let model = m,
         view = v,
         $event_pump = $('body'),
+        $person_id = $('#person_id'),
         $fname = $('#fname'),
         $lname = $('#lname');
 
@@ -157,35 +160,46 @@ ns.controller = (function (m, v) {
         e.preventDefault();
 
         if (validate(fname, lname)) {
-            model.create(fname, lname)
+            model.create({
+                'fname': fname,
+                'lname': lname,
+            })
         } else {
             alert("Problem with first or last name input");
         }
     });
 
     $('#update').click(function (e) {
-        let fname = $fname.val(),
+        let person_id = $person_id.val(),
+            fname = $fname.val(),
             lname = $lname.val();
 
         e.preventDefault();
 
         if (validate(fname, lname)) {
-            model.update(fname, lname)
+            model.update({
+                person_id: person_id,
+                fname: fname,
+                lname: lname,
+            })
         } else {
             alert("Problem with first or last name input");
         }
+        e.preventDefault();
     });
 
     $('#delete').click(function (e) {
         let lname = $lname.val();
+        let person_id = $person_id.val();
 
         e.preventDefault();
 
         if (validate('blah', lname)) {
-            model.delete(lname)
+            model.delete(person_id)
         } else {
             alert("Problem with last name input");
         }
+        e.preventDefault();
     });
 
     $('#reset').click(function () {
@@ -194,8 +208,13 @@ ns.controller = (function (m, v) {
 
     $('table > tbody').on('dblclick', 'tr', function (e) {
         let $target = $(e.target),
+            person_id,
             fname,
             lname;
+
+        person_id = $target
+            .parent()
+            .attr('data-person-id');
 
         fname = $target
             .parent()
@@ -207,7 +226,11 @@ ns.controller = (function (m, v) {
             .find('td.lname')
             .text();
 
-        view.update_editor(fname, lname);
+        view.update_editor({
+            person_id: person_id,
+            fname: fname,
+            lname: lname,
+        });
     });
 
     // Handle the model events
